a00bd4c57b3c1399d13e70ccf45ed306a0afabdf
felixghislaindev <felixghislain@yahoo.com>
2025-03-25 14:02:52 +0000

    setting up fav.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
+      {/* Favicon links */}
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
